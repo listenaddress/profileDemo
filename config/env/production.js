d784fd71d00f258bfab6a658228fbd645a34a4b6
Thomas O'Brien <tbobrien612@gmail.com>
2015-07-21 20:56:40 -0500

    db

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+	db: 'mongodb://tom:reptie@ds053728.mongolab.com:53728/profile',
 	assets: {
 		lib: {
 			css: [
